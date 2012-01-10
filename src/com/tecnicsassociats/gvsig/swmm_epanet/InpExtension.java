4c4e13e2c5af65d373d65abe70e447006ab32aaf
David Erill <daviderill79@gmail.com>
2012-01-10 20:39:55 +0100

    Information files and copyright

--- src/com/tecnicsassociats/gvsig/swmm_epanet/InpExtension.java
+++ src/com/tecnicsassociats/gvsig/swmm_epanet/InpExtension.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of swmm_epanet
+ * Copyright (C) 2012  Tecnics Associats
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author:
+ *   David Erill <daviderill79@gmail.com>
+ */
+
 package com.tecnicsassociats.gvsig.swmm_epanet;
 
 import java.awt.geom.PathIterator;
